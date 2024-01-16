d545fe8da39053da22bed50b3e1a1668bfea6941
Mikhail Puzanov <mpuzanov@mail.ru>
2024-01-16 17:02:47 +0400

    установка логгера по умолчанию

--- main.go
+++ main.go
@@ -6,24 +6,40 @@ import (
 	"kvart-info/internal/services"
 	"kvart-info/internal/storage"
 	"log/slog"
+	"os"
 )
 
 func main() {
-	cfg, err := config.LoadConfig("config.json")
-	if err != nil {
-		panic(err)
-	}
-	// TODO: установка логгера по умолчанию на основании конфиг файла
-	//slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	cfg := config.MustConfig("config.json")
 
-	slog.Debug("debug", "cfg", cfg)
+	//установка логгера по умолчанию на основании конфиг файла
+	logger := NewLogger(cfg.Env)
+	slog.SetDefault(logger)
+
+	logger.Debug("debug", "cfg", cfg)
 	db, err := storage.NewDB(cfg)
 	if err != nil {
 		panic(err)
 	}
 
-	if err = services.New(cfg, db).GetLicTotal(); err != nil {
+	if err = services.New(cfg, db).Run(); err != nil {
 		panic(err)
 	}
 
+}
+
+// NewLogger ...
+func NewLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "dev":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }

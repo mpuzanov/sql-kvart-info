14ed0bd9ec622b3e920031abf65ea79e1a14d8af
Mikhail Puzanov <mpuzanov@mail.ru>
2024-01-17 15:46:40 +0400

    + log to file

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"io"
 	"kvart-info/internal/config"
 	"kvart-info/internal/services"
 	"kvart-info/internal/storage"
 	"log/slog"
 	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
@@ -39,8 +42,18 @@ func NewLogger(env string) *slog.Logger {
 	case "dev":
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case "prod":
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	}
+		// добавим запись логов в файл
+		ex, _ := os.Executable()
+		fileName := filepath.Base(ex)
+		fileLog := strings.TrimSuffix(fileName, filepath.Ext(fileName)) + ".log"
+		workDir := filepath.Dir(ex) // путь к программе
+		fileLog = filepath.Join(workDir, fileLog)
 
+		file, _ := os.OpenFile(fileLog, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		multi := io.MultiWriter(file, os.Stdout) //, os.Stderr
+
+		log = slog.New(slog.NewJSONHandler(multi, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	log = log.With(slog.String("env", env))
 	return log
 }
